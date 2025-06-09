ac434d288a65cbc816ed2de5058063aa2e1d1d52
AlfonsoRM100 <alfonsorivasmelin@gmail.com>
2025-06-09 19:49:01 -0400

    clase_1

---
+++ tipos_datos/datos_simples.py
@@ -0,0 +1,31 @@
+#datos de tipo cadena de texto
+cadena_de_texto_1 = 'cadena de texto'
+cadena_de_texto_2 = "otra cadena de texto"
+cadena_de_texto_3 = """este es una tercera
+cadena de caracteres 
+de multiples lineas"""
+
+print('esto es una cadena de texto')
+print('esto es una cadena de texto con "comillas dobles"')
+print("esto es una cadena de texto con 'comillas simples'")
+
+# Datos de tipo numerico
+numero_entero=49
+# float
+numero_decimal= 56.32
+
+# Datos de tipo logico
+#bool
+dato_verdadero = True
+dato_falso = False
+
+
+print(cadena_de_texto_1)
+print(cadena_de_texto_2)
+print(cadena_de_texto_3)
+
+
+saludo = "Buen dia"
+nombre = input("Escriba su nombre: ")
+print(texto final)
+
