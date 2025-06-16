dcb6c82180c82b6c9266a8e39c9773b60f122888
AlfonsoRM100 <alfonsorivasmelin@gmail.com>
2025-06-16 19:57:53 -0400

    cambios

---
+++ Operadores/op_comparacion.py
@@ -0,0 +1,14 @@
+# operador Equivalente 
+print('operador equivalente')
+print('5 == 4',5 == 4 )
+print('5 ==5', 5 == 5)
+ 
+ # OPERADOR  EXCLUSIVAMENTE MAYOR QUE 
+print('operador mayor que')
+print('5 > 4', 5 > 4)
+print('5 > 8', 5 > 8)
+
+ #OPERADOR  EXCLUSIVAMENTE MENOR QUE
+ print('operador menor que')
+print('5 < 4', 5 < 4)
+print('')
---
+++ Operadores/op_logicos.py
@@ -0,0 +1,13 @@
+# OPERADOR O
+print('operador O u OR')
+print('v o v',True | True )
+print('v o f',True | False)
+print('f o v',False | True)
+print('f o f',False | False)
+
+# OPERADOR Y
+print('operador O u OR')
+print('v o v',True | True )
+print('v o f',True | False)
+print('f o v',False | True)
+print('f o f',False | False)
