f657d5f79dd505fe3fda9167ca98772c703cee06
AlfonsoRM100 <alfonsorivasmelin@gmail.com>
2025-06-16 19:57:08 -0400

    cambio

---
+++ Metodos/Metodos_cadenas.py
@@ -0,0 +1,9 @@
+# UN METODO ES UNA FUNCION DE PROGRAMACION QUE EJECUTA UNA TAREA ESPECIFICA
+
+
+# EL METODO DIR NOS INDICA TODOS LOS METODOS DISPONIBLES CON EL TIPO DE DATOS
+print(dir(str))
+print(nombre.capitalize())
+print(nombre.title())
+print(nombre.upper())
+print(asignatura.lower())
