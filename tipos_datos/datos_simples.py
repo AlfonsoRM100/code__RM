307f62d468bd7c02413ff988eeda0ac836f6e56f
AlfonsoRM100 <alfonsorivasmelin@gmail.com>
2025-06-16 19:54:20 -0400

    cambios

--- tipos_datos/datos_simples.py
+++ tipos_datos/datos_simples.py
@@ -1,31 +1,43 @@
-#datos de tipo cadena de texto
-cadena_de_texto_1 = 'cadena de texto'
-cadena_de_texto_2 = "otra cadena de texto"
-cadena_de_texto_3 = """este es una tercera
-cadena de caracteres 
-de multiples lineas"""
+# Datos de tipo cadena de texto
+# string
+cadena_de_texto_1 = 'Cadena de texto'
+cadena_de_texto_2 = "Otra cadena de texto"
+cadena_de_texto_3 = """Este es una tercera 
+cadena de caracteres
+de múltiples líneas"""
 
-print('esto es una cadena de texto')
-print('esto es una cadena de texto con "comillas dobles"')
-print("esto es una cadena de texto con 'comillas simples'")
+print('Esto es una cadena de texto')
+print('Esto es una cadena de texto con "comillas dobles"')
+print("Esto es una cadena de texto con 'comillas simples'")
 
-# Datos de tipo numerico
-numero_entero=49
+# Datos de tipo numérico
+# int
+numero_entero = 49
 # float
-numero_decimal= 56.32
+numero_decimal = 56.32
 
-# Datos de tipo logico
-#bool
+# Datos de tipo lógico
+# bool
 dato_verdadero = True
 dato_falso = False
-
 
 print(cadena_de_texto_1)
 print(cadena_de_texto_2)
 print(cadena_de_texto_3)
 
-
-saludo = "Buen dia"
+saludo = "Buen día"
 nombre = input("Escriba su nombre: ")
-print(texto final)
+texto_final = saludo + " " + nombre
+print(texto_final)
 
+numero_1 = 35
+numero_2 = 12.63
+print(numero_1 * numero_2)
+
+print(dato_falso)
+
+print(type(cadena_de_texto_1))
+print(len(cadena_de_texto_1))
+print(type(numero_entero))
+print(type(numero_decimal))
+print(type(dato_verdadero))
